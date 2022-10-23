b17399270f70b7b688889b63e3cc16042e0bee7e
Efrat Weksler <eweksler@g.jct.ac.il>
2022-10-23 14:45:07 +0300

    initial code and Welcome functions

---
+++ Stage0/Program1765.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stage0
+    {
+        partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome1765();
+            welcome6768();
+            Console.ReadKey();
+        }
+
+        static partial void welcome6768();
+        private static void welcome1765()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}

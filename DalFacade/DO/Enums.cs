88c239a1b0c09b541838bda24332843f1fc864c1
Efrat Weksler <eweksler@g.jct.ac.il>
2022-11-02 17:34:39 +0200

    public enum

--- DalFacade/DO/Enums.cs
+++ DalFacade/DO/Enums.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+namespace DO;
 
-namespace DO
-{
-  enum category { Prints, Apparel, Stickers, Notebooks, Mugs };
-  enum status { active, nonActive };
- // enum size { xs, s, m, l, xl };
-}
+public enum category { Prints, Apparel, Stickers, Notebooks, Mugs };
+public enum status { active, nonActive };
+

75c84844bd84bf0403e2e2aae082860faaa61eed
Tzipora Maroko <maroko@g.jct.ac.il>
2022-11-24 20:51:52 +0200

    ibl

--- BL/BlImplementation/Bl.cs
+++ BL/BlImplementation/Bl.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlApi;
 
 using BO;
-namespace BlApi;
+namespace BlImplementation;
 
 sealed public class Bl : IBl
 {
-    public IProduct Product  => new Product();
+    public IProduct Product => new Product();
     public IOrder Order => new Order();
     public ICart Cart => new Cart();
 }

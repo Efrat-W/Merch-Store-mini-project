e5cd2e44aac1c00f35a9e4f7991e4b191ecf2260
Tzipora Maroko <maroko@g.jct.ac.il>
2022-11-26 21:09:55 +0200

    main

--- BL/BO/Enums.cs
+++ BL/BO/Enums.cs
@@ -11,3 +11,21 @@ public enum orderStatus
 {
    Approved, Shipped, Delivered
 }
+public enum menu
+{
+    Exit, Product, Order, Cart
+}
+
+public enum optionsProduct
+{
+    Return, Add, ShowByIdMan, ShowByIdCus,Update, Delete
+}
+public enum optionsOrder
+{
+    Return, Track , ShowById, UpdateShipment, UpdateDelivery, RequestAll
+}
+
+public enum optionsCart
+{
+    Return, Add, UpdateAmount, Approve
+}
--- BlTest/Program.cs
+++ BlTest/Program.cs
@@ -1,7 +1,10 @@
 ﻿using BlApi;
 using BlImplementation;
+using BO;
 using Dal;
 using DO;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace BlTest
 {
@@ -10,7 +13,7 @@ namespace BlTest
         static private IBl bl = new Bl();
         static void Main(string[] args)
         {
-            menu choice = new menu();
+            BO.menu choice = new BO.menu();
             do
             {
                 Program program = new Program();
@@ -18,80 +21,178 @@ namespace BlTest
     0: Exit
     1: Product
     2: Order
-    3: Order Item");
+    3: Cart");
                 string input = Console.ReadLine();
-                choice = (menu)Enum.Parse(typeof(menu), input);
+                choice = (BO.menu)Enum.Parse(typeof(BO.menu), input);
 
                 switch (choice)
                 {
-                    case menu.Product:
+                    case BO.menu.Product:
                         program.ProductMenu();
                         break;
-                    case menu.Order:
+                    case BO.menu.Order:
                         program.OrderMenu();
                         break;
-                    case menu.OrderItem:
+                    case BO.menu.Cart:
                         program.CartMenu();
                         break;
                     default:
                         break;
                 }
-            } while (choice != menu.Exit);
+            } while (choice != BO.menu.Exit);
         }
 
         private void ProductMenu()
         {
-            options op = new options();
+            BO.optionsProduct op = new BO.optionsProduct();
             Console.WriteLine(@"Enter one of the following:
     0: return to main menu
     1: add product
-    2: print product (by id)
-    3: print the list of products
-    4: update a certain product
+    2: print product (by id) for manager
+    3: print product (by id) for customer
+    4: update a product
     5: delete a product");
 
             string input = Console.ReadLine();
-            op = (options)Enum.Parse(typeof(options), input);
+            op = (BO.optionsProduct)Enum.Parse(typeof(BO.optionsProduct), input);
             do
             {
                 switch (op)
                 {
-                    case options.Add:
+                    case BO.optionsProduct.Add:
                         Console.WriteLine("Enter ID, Name, Price, category and the amount in stock");
                         bl.Product.Add(InitializeProduct());
                         break;
-                    case options.ShowById:
+                    case BO.optionsProduct.ShowByIdMan:
                         Console.WriteLine("Enter ID");
                         int.TryParse(Console.ReadLine(), out int id);
-                        Console.WriteLine(bl.Product.RequestById(id)); //print requested product to console
+                        Console.WriteLine(bl.Product.RequestByIdManager(id)); //print requested product to console
                         break;
-                    case options.ShowList:
-                        IEnumerable<Product> productList = bl.Product.RequestAll();
-                        foreach (Product prod in productList)
-                            Console.WriteLine(prod); ;
+                    case BO.optionsProduct.ShowByIdCus:
+                        Console.WriteLine("Enter ID");
+                        int.TryParse(Console.ReadLine(), out id);
+                        Console.WriteLine("Enter customer name, email, adress and the number of items in your cart:");
+                        Console.WriteLine(bl.Product.RequestByIdCustomer(id, InitializeCart())); 
                         break;
-                    case options.Update:
+                    case BO.optionsProduct.Update:
                         Console.WriteLine("Enter the existing product's ID");
                         id = int.Parse(Console.ReadLine());
-                        Product update = bl.Product.RequestById(id);
+                        BO.Product update = bl.Product.RequestByIdManager(id);
                         Console.WriteLine(update);
                         Console.WriteLine("Enter the new Name, Price, category and the amount in stock");
                         bl.Product.Update(InitializeProduct(id));
                         break;
-                    case options.DeleteFromList:
+                    case BO.optionsProduct.Delete:
                         Console.WriteLine("Enter the ID of the product you wish to remove ");
-                        Product product = new Product() { ID = int.Parse(Console.ReadLine()) };
-                        bl.Product.Delete(product);
+                        int.TryParse(Console.ReadLine(), out id);
+                        bl.Product.Delete(id);
                         break;
                     default:
                         break;
                 }
                 //re-ask for user input
                 input = Console.ReadLine();
-                op = (options)Enum.Parse(typeof(options), input);
-            } while (op != options.Return);
+                op = (BO.optionsProduct)Enum.Parse(typeof(BO.optionsProduct), input);
+            } while (op != BO.optionsProduct.Return);
         }
-    
+        private void OrderMenu()
+        {
+            BO.optionsOrder op = new BO.optionsOrder();
+            Console.WriteLine(@"Enter one of the following:
+    0: return to main menu
+    1: track order
+    2: print order (by id)
+    3: print the list of orders
+    4: update shipment
+    5: update delivery
+    6: print all orders");
+            string input = Console.ReadLine();
+            op = (BO.optionsOrder)Enum.Parse(typeof(BO.optionsOrder), input);
+            do
+            {
+                switch (op)
+                {
+                    case BO.optionsOrder.Track:
+                        Console.WriteLine("Enter id:");
+                        input = Console.ReadLine();
+                        int.TryParse(input, out int id);
+                        Console.WriteLine(bl.Order.Track(id));
+                        break;
+                    case BO.optionsOrder.ShowById:
+                        Console.WriteLine("Enter ID");
+                        input = Console.ReadLine();
+                        int.TryParse(input, out  id);
+                        Console.WriteLine(bl.Order.RequestById(id));
+                        break;
+                    case BO.optionsOrder.UpdateShipment:
+                        Console.WriteLine("Enter ID");
+                        input = Console.ReadLine();
+                        int.TryParse(input, out id);
+                        Console.WriteLine(bl.Order.UpdateShipment(id));
+                        break;
+                    case BO.optionsOrder.UpdateDelivery:
+                        Console.WriteLine("Enter ID");
+                        input = Console.ReadLine();
+                        int.TryParse(input, out id);
+                        Console.WriteLine(bl.Order.UpdateShipment(id));
+                        break;
+                    case BO.optionsOrder.RequestAll:
+                        IEnumerable<BO.OrderForList> list = bl.Order.RequestOrders();
+                        foreach (BO.OrderForList item in list)
+                            Console.WriteLine(item);
+                        break;
+                    default:
+                        break;
+                }
+                //re-ask for user input
+                input = Console.ReadLine();
+                op = (BO.optionsOrder)Enum.Parse(typeof(BO.optionsOrder), input);
+            } while (op != BO.optionsOrder.Return);
+        }
+
+        private void CartMenu()
+        {
+            BO.optionsCart op = new BO.optionsCart();
+            Console.WriteLine(@"Enter one of the following:
+    0: return to main menu
+    1: add product
+    2: change product's amount
+    3: approve the order");
+            string input = Console.ReadLine();
+            op = (BO.optionsCart)Enum.Parse(typeof(BO.optionsCart), input);
+            do
+            {
+                switch (op)
+                {
+                    case BO.optionsCart.Add:
+                        Console.WriteLine("Enter product's id:");
+                        input = Console.ReadLine();
+                        int.TryParse(input, out int id);
+                        Console.WriteLine("Enter customer name, email, adress and the number of items in your cart:");
+                        Console.WriteLine(bl.Cart.AddProduct(InitializeCart(),id));
+                        break;
+                    case BO.optionsCart.UpdateAmount:
+                        Console.WriteLine("Enter product's id and the new amount (- or +)");
+                        input = Console.ReadLine();
+                        int.TryParse(input, out int prodId);
+                        input = Console.ReadLine();
+                        int.TryParse(input, out int amount);
+                        Console.WriteLine("Enter customer name, email, adress and the number of items in your cart:");
+                        Console.WriteLine(bl.Cart.UpdateProductAmount(InitializeCart(),prodId, amount));
+                        break;
+                    case BO.optionsCart.Approve:
+                        Console.WriteLine("Enter customer name, email, adress and the number of items in your cart:");
+                        Console.WriteLine(bl.Cart.Approve(InitializeCart()));
+                        break;
+                    default:
+                        break;
+                }
+                //re-ask for user input
+                input = Console.ReadLine();
+                op = (BO.optionsCart)Enum.Parse(typeof(BO.optionsCart), input);
+            } while (op != BO.optionsCart.Return);
+        }
+
         static BO.Product InitializeProduct(int updateId = 0)
         {
             int id = updateId;
@@ -109,8 +210,53 @@ namespace BlTest
             input = Console.ReadLine();
             int.TryParse(input, out int inStock);
 
-            BO.Product product = new BO.Product() { ID = id, Category = category, InStock = inStock, Name = name, Price = price };
-            return product;
+            return new BO.Product() { ID = id, Category = category, InStock = inStock, Name = name, Price = price };
+        }
+        static BO.Cart InitializeCart()
+        {
+            string input = Console.ReadLine();
+            string name = input;
+            input = Console.ReadLine();
+            string email = input;
+            input = Console.ReadLine();
+            string adress = input;
+            int num;
+            input = Console.ReadLine();
+            int.TryParse(input, out num);
+            List<BO.OrderItem> Items= new List<BO.OrderItem>();
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine("Enter name, product id, price and amount");
+                Items.Add(InitializeOrderItem());
+            }
+            double total = Items.Sum(i => i.Price * i.Amount);
+            return new BO.Cart() { 
+                CustomerName=name,
+                CustomerEmail=email,
+                CustomerAddress=adress,
+                Items=Items,
+                TotalPrice=total,
+            };           
+        }
+        static BO.OrderItem InitializeOrderItem()
+        {
+            int prodId, price, amount;
+            string input = Console.ReadLine();
+            string name = input;
+            input = Console.ReadLine();
+            int.TryParse(input, out prodId);
+            input = Console.ReadLine();
+            int.TryParse(input, out price);
+            input = Console.ReadLine();
+            int.TryParse(input, out amount);
+           return new BO.OrderItem()
+            {
+               Name = name,
+               ProductId = prodId,
+               Price = price,   
+               Amount = amount,
+               TotalPrice=amount*price,
+            };
         }
     }
 }

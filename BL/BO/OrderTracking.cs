9dd45c92a6d2a068fdecfba74f92ea8c793ff5f0
Efrat Weksler <eweksler@g.jct.ac.il>
2022-11-27 01:41:50 +0200

    .

--- BL/BO/OrderTracking.cs
+++ BL/BO/OrderTracking.cs
@@ -1,5 +1,9 @@
 ﻿
 
+using DO;
+using System.Diagnostics;
+using System.Xml.Linq;
+
 namespace BO;
 
 public class OrderTracking
@@ -7,4 +11,18 @@ public class OrderTracking
     public int ID { get; set; }
     public orderStatus Status { get; set; }
     public List<Tuple<DateTime, string>> orderProgress { get; set; }
+
+    public override string ToString()
+    {
+        string s = $@"
+    Order id: {ID}
+    Status: {Status}
+    ";
+        foreach (var tuple in orderProgress)
+        {
+            s += $"at {tuple.Item1}: {tuple.Item2}\n";
+        }
+        return s;
+    }
+
 }

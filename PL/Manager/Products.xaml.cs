9376294d62eef933f6efb59ca22cf87fa7aa117c
Efrat Weksler <eweksler@g.jct.ac.il>
2022-12-11 12:54:32 +0200

    rough  update listview

--- PL/Manager/Products.xaml.cs
+++ PL/Manager/Products.xaml.cs
@@ -38,15 +38,17 @@ namespace PL.Manager
         }
 
         private void AddBtn_Click(object sender, RoutedEventArgs e) {
-            new Product().Show();
-            ProductsListView.ItemsSource = bl.Product.RequestList();
+            new Product().ShowDialog();
+            ProductsListView.Items.Refresh();
+           // ProductsListView.ItemsSource = bl.Product.RequestListByCond(i => i.Category == (BO.category)CategorySelector.SelectedItem);
+
         }
 
         private void ProductsListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             int id = ((ProductForList)ProductsListView.SelectedItem).ID;
-            new Product(id).Show();
-            ProductsListView.ItemsSource = bl.Product.RequestList();
+            new Product(id).ShowDialog();
+            ProductsListView.Items.Refresh();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)

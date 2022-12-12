28f1f5c44fc0cd0f1a0dfe5c844f04e17c72d6e8
Tzipora Maroko <maroko@g.jct.ac.il>
2022-12-12 21:49:37 +0200

    ...

--- DalFacade/DalApi/DalConfig.cs
+++ DalFacade/DalApi/DalConfig.cs
@@ -14,7 +14,7 @@ static class DalConfig
 
     static DalConfig()
     {
-        XElement dalConfig = XElement.Load(@"xml\dal-config.xml")
+        XElement dalConfig = XElement.Load(@"..\xml\dal-config.xml")
             ?? throw new DalConfigException("dal-config.xml file is not found");
         s_dalName = dalConfig?.Element("dal")?.Value
             ?? throw new DalConfigException("<dal> element is missing");

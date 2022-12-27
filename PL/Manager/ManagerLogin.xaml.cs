5ff357c2ea4c02a13439f3d1da7b762e086abbb9
Efrat Weksler <eweksler@g.jct.ac.il>
2022-12-27 17:43:20 +0200

    updated manager window, missing regex and hiding of input

--- PL/Manager/ManagerLogin.xaml.cs
+++ PL/Manager/ManagerLogin.xaml.cs
@@ -21,7 +21,8 @@ namespace PL.Manager
     public partial class ManagerLogin : Window
     {
         BlApi.IBl? bl = BlApi.Factory.Get();
-        const int PASSWORD = 123;
+        const string PASSWORD = "123";
+        string currentPassword = "";
         public ManagerLogin()
         {
             InitializeComponent();
@@ -29,11 +30,9 @@ namespace PL.Manager
 
         private void CommandBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (int.Parse(PasswordTB.Text) == PASSWORD)
-            {
-                Login.Visibility = Visibility.Hidden;
-                LoggedIn.Visibility = Visibility.Visible;
-            }
+            if (currentPassword == PASSWORD)
+                Toggle();
+            PasswordTB.Text = "";
         }
 
         private void ProductViewButton_Click(object sender, RoutedEventArgs e)
@@ -46,6 +45,57 @@ namespace PL.Manager
         {
             Orders orders = new(bl);
             orders.Show();
+        }
+
+        private void PasswordTB_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                if (currentPassword == PASSWORD)
+                    Toggle();
+            }
+            else
+            {
+                //regex needed
+                
+            }
+        }
+
+        private void Toggle()
+        {
+            if (Login.Visibility == Visibility.Hidden)
+            {
+                LoggedIn.Visibility = Visibility.Hidden;
+                Login.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                Login.Visibility = Visibility.Hidden;
+                LoggedIn.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void logOutText_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Toggle();
+        }
+
+
+        private void logOutText_MouseEnter(object sender, MouseEventArgs e)
+        {
+            logOutText.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void logOutText_MouseLeave(object sender, MouseEventArgs e)
+        {
+            logOutText.TextDecorations = null;
+        }
+
+        private void PasswordTB_KeyUp(object sender, KeyEventArgs e)
+        {
+            char inputKey = PasswordTB.Text.ElementAt(PasswordTB.Text.Length - 1);
+            currentPassword += inputKey;
+            PasswordTB.Text.Replace(inputKey, '•');
         }
     }
 }
